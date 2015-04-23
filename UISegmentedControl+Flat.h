08a77e937bf8b61a1bb10f4d0859d79f402a5d1d
Mayank <gupta.mayank@99acres.com>
2015-04-24 00:12:34 +0530

    Initial Commit

---
+++ UISegmentedControl+Flat.h
@@ -0,0 +1,15 @@
+//
+//  UISegmentedControl+Flat.h
+//  99acres
+//
+//  Created by Mayank on 20/08/14.
+//  Copyright (c) 2014 InfoEdge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISegmentedControl (Flat)
+
+- (void)setupFlat:(BOOL)isSegmentToBeGray;
+
+@end
